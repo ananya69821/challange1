ce1e8e8fd80543bf63403889fed4e116a562da71
ananya69821 <ananya.sakshi.srivastava@gmail.com>
2023-03-15 16:44:59 -0400

    Delete seeddb.tf

--- Task1/seeddb.tf
+++
@@ -1,9 +0,0 @@
-# resource "null_resource" "database_setup" {
-#   provisioner "local-exec" {
-#     command = "sqlcmd -S ${azurerm_mysql_server.sqlServer1.fully_qualified_domain_name} -U ${azurerm_mysql_server.sqlserver1.administrator_login} -P ${azurerm_mysql_server.sqlserver1.administrator_login_password} -d azurerm_mysql_database.sqlDB1.name -i 01.sql"
-    
-#   }
-#   depends_on = [
-#     azurerm_mysql_database.sqlDB1
-#   ]
-# }
